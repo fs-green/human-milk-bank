30e1089c22d617294a09fec8be29cbec03ebce26
fs-green <fs-green@posteo.at>
2020-06-09 22:17:31 +0200

    added header, login component

--- human-milk-bank-frontend/src/main/web/src/app/app.module.ts
+++ human-milk-bank-frontend/src/main/web/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { AppMaterialModule } from './app-material/app-material.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
+
+//TODO wie funktioniert das nochmal mit den modulen???
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
