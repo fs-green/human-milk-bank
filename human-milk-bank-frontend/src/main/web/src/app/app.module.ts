be185edf42696d69acb84585e7dcafb692bc511d
fs-green <fs-green@posteo.at>
2020-06-15 22:37:04 +0200

    HMB-000 [fs-green] added comments

--- human-milk-bank-frontend/src/main/web/src/app/app.module.ts
+++ human-milk-bank-frontend/src/main/web/src/app/app.module.ts
@@ -10,9 +10,14 @@ import { AppMaterialModule } from './app-material/app-material.module';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 
+// Nach den Import-Anweisungen folgt der Decorator @NgModule. Hier findet eine deklarative Konfiguration eines
+// Angular-Moduls statt. Mit Hilfe von Modulen werden in der Angular-Plattform zusammengehörige Komponenten und Services
+// gekapselt. Dabei besteht jede Angular-Anwendung aus mindestens einem Modul, nämlich konkret dem Applikationsmodul.
+// Folgende Angaben sind verpflichtend:
 
-//TODO wie funktioniert das nochmal mit den modulen???
 @NgModule({
+// Abschnitt declarations: Alle hier aufgeführten Komponenten sind im gesamten Applikationsmodul sichtbar und können
+// von jeder anderen Komponente verwendet werden. Wir haben im Moment nur eine Komponente namens AppComponent.
   declarations: [
     AppComponent,
     HomeComponent,
@@ -20,6 +25,11 @@ import { HeaderComponent } from './header/header.component';
     LoginComponent
   ],
   imports: [
+// Die Angabe, welches Zielsystem angesprochen werden soll: imports: [BrowserModule].
+// In diesem Fall möchten wir die Applikation in einem Webbrowser laufen lassen. Wir verwenden vorerst nur diesen
+// Applikationstyp. Wir haben schon darauf hingewiesen, dass mit Angular auch Applikationen für andere Systeme, wie etwa
+// mobile Geräte, möglich sind. In diesem Fall müssten wir andere Module an dieser Stelle importieren. Wir stellen
+// diese Möglichkeiten hier jedoch ausdrücklich zurück.
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
@@ -27,6 +37,7 @@ import { HeaderComponent } from './header/header.component';
     AppMaterialModule
   ],
   providers: [],
+// Die Einstiegskomponente in die Applikation: bootstrap: [AppComponent], also der Entry Point der Applikation
   bootstrap: [AppComponent]
 })
 export class AppModule { }
