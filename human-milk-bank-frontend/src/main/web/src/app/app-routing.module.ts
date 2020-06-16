edf7b3d1e29471996517f22c49ce74c803093e7c
fs-green <fs-green@posteo.at>
2020-06-16 07:46:49 +0200

    comments

--- human-milk-bank-frontend/src/main/web/src/app/app-routing.module.ts
+++ human-milk-bank-frontend/src/main/web/src/app/app-routing.module.ts
@@ -4,12 +4,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
+/**
+ * Hier wird das Routing festgelegt. In diesem Beispiel wird beim leeren path in der URL auf
+ * die Komponente HomeComponent verwiesen. Bei Login auf LoginComponent und bei irgendeinem
+ * anderen Wert auf leer, also wieder auf HomeComponent verwiesen.
+ */
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: ''}
 ];
 
+/**
+ * Hier wird die AppRoutingModule als Angular Komponente gehandelt und beinhaltet lediglich
+ * die Routes der Applikation
+ */
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- human-milk-bank-frontend/src/main/web/src/app/app.component.ts
+++ human-milk-bank-frontend/src/main/web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 /**
- * Mit Hilfe eines Decorators werden Klassen um weitere Informationen, d. h. um Metadaten erweitert. 
- * Über die Eigenschaft selector zeigen Sie an, an welcher Stelle die Komponente verwendet werden soll. 
- * Über die template-Eigenschaft konfigurieren Sie schließlich, dass bei der Instanziierung der Komponente 
- * das hinterlegte Template gerendert werden soll. Ebenso verweisen Sie auf die zugehörige CSS-Datei für das Styling der Komponente. 
+ * Mit Hilfe eines Decorators werden Klassen um weitere Informationen, d. h. um Metadaten 
+ * erweitert. Über die Eigenschaft selector zeigen Sie an, an welcher Stelle die Komponente 
+ * verwendet werden soll. Über die template-Eigenschaft konfigurieren Sie schließlich, dass bei 
+ * der Instanziierung der Komponente das hinterlegte Template gerendert werden soll. 
+ * Ebenso verweisen Sie auf die zugehörige CSS-Datei für das Styling der Komponente. 
  */
 @Component({
   selector: 'app-root',
